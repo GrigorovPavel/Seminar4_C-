7044d301bd40defd4f524eb2dbaf844846b97abc
Pavel <grigorov_ps@mail.ru>
2023-10-20 17:14:05 +0300

    Создали программу Task28 (Произведение от 1 до N)- Факториал

---
+++ Task28/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая
+// 1. принимает на вход числоа и  
+// 2. выдает произведение чисел от 1 до А
+// 4->24
+// 5->120
+
+int MultNumbers(int num)
+{
+    int count = 1;
+
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+          count = count * i;  
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 1)  //проверка на натуральность числа
+{
+    Console.WriteLine("Некорректный ввод");
+    return; // Завершает МЕТОД и возвращает к месту вызова МЕТОДА
+}
+
+int multNumber = MultNumbers(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {multNumber}");
