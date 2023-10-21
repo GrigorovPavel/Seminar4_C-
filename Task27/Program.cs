dbb8e5c303a78a273c8ce4dde0d207b5801708d6
Pavel <grigorov_ps@mail.ru>
2023-10-21 15:21:58 +0300

    Создали проект Task27 (Выдает сумму цифр в числе)

---
+++ Task27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая 
+// принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumDigit(int num)
+{
+    int count = 0;
+    int temp = 0;
+    while(count < num)
+    {
+        temp = temp + num % 10;
+        num = num / 10;
+        count++;
+    }
+    return temp;
+}
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigit = SumDigit(number);
+Console.WriteLine($"Cумма цифр в числе {number} -> {sumDigit}");
